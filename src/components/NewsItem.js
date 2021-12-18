b37f5148b367b574503bba5e0c955a4971359361
Paarth Kothari <paarthkothari14@gmail.com>
2021-12-18 18:47:33 +0530

    Adding Previous & Next Buttons to populate NewsItems

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -16,12 +16,13 @@ export class NewsItem extends Component {
             alt="..."
           />
           <div className="card-body">
-            <h5 className="card-title">{title}...</h5>
-            <p className="card-text">{description}...</p>
+            <h5 className="card-title">{title}</h5>
+            <p className="card-text">{description}</p>
             <a
+              rel="noreferrer"
               href={newsUrl}
               target="_blank"
-              className="btn btn-sm btn-primary"
+              className="btn btn-sm btn-dark"
             >
               Read More
             </a>
