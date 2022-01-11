8f46710404a26911c457839c5a4fad921e56fbd2
Paarth Kothari <paarthkothari14@gmail.com>
2022-01-11 14:31:39 +0530

    Refactored News Component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import News from "./components/News";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 export default class App extends Component {
+  pageSize = 6;
   render() {
     return (
       <div className="text-center">
@@ -13,7 +14,7 @@ export default class App extends Component {
             <Route exact path="/">
               <News
                 key="general"
-                pageSize={5}
+                pageSize={this.pageSize}
                 country="in"
                 category="general"
               />
@@ -21,7 +22,7 @@ export default class App extends Component {
             <Route exact path="/business">
               <News
                 key="business"
-                pageSize={5}
+                pageSize={this.pageSize}
                 country="in"
                 category="business"
               />
@@ -29,7 +30,7 @@ export default class App extends Component {
             <Route exact path="/entertainment">
               <News
                 key="entertainment"
-                pageSize={5}
+                pageSize={this.pageSize}
                 country="in"
                 category="entertainment"
               />
@@ -37,29 +38,39 @@ export default class App extends Component {
             <Route exact path="/general">
               <News
                 key="general"
-                pageSize={5}
+                pageSize={this.pageSize}
                 country="in"
                 category="general"
               />
             </Route>
             <Route exact path="/health">
-              <News key="health" pageSize={5} country="in" category="health" />
+              <News
+                key="health"
+                pageSize={this.pageSize}
+                country="in"
+                category="health"
+              />
             </Route>
             <Route exact path="/science">
               <News
                 key="science"
-                pageSize={5}
+                pageSize={this.pageSize}
                 country="in"
                 category="science"
               />
             </Route>
             <Route exact path="/sports">
-              <News key="sports" pageSize={5} country="in" category="sports" />
+              <News
+                key="sports"
+                pageSize={this.pageSize}
+                country="in"
+                category="sports"
+              />
             </Route>
             <Route exact path="/technology">
               <News
                 key="technology"
-                pageSize={5}
+                pageSize={this.pageSize}
                 country="in"
                 category="technology"
               />
--- src/components/News.js
+++ src/components/News.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import NewsItem from "./NewsItem";
 import Spinner from "./Spinner";
-import PropTypes from "prop-types";
+import propTypes from "prop-types";
 
 export class News extends Component {
   static defaultProps = {
@@ -10,10 +10,10 @@ export class News extends Component {
     category: "General",
   };
 
-  static PropTypes = {
-    country: PropTypes.string,
-    pageSize: PropTypes.number,
-    category: PropTypes.string,
+  static propTypes = {
+    country: propTypes.string,
+    pageSize: propTypes.number,
+    category: propTypes.string,
   };
 
   constructor() {
@@ -26,9 +26,9 @@ export class News extends Component {
     };
   }
 
-  async componentDidMount() {
+  async updateNews(pageNo) {
     // console.log("cdm");
-    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=143400755624442599d36dc7e12efca4&page=1&pageSize=${this.props.pageSize}`;
+    const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=143400755624442599d36dc7e12efca4&page=${this.state.page}&pageSize=${this.props.pageSize}`;
     this.setState({ loading: true });
     let data = await fetch(url);
     let parsedData = await data.json();
@@ -40,51 +40,18 @@ export class News extends Component {
     });
   }
 
+  async componentDidMount() {
+    this.updateNews();
+  }
+
   handlePrevClick = async () => {
-    console.log("Previous");
-    let url = `https://newsapi.org/v2/top-headlines?country=${
-      this.props.country
-    }&category=${
-      this.props.category
-    }&apiKey=143400755624442599d36dc7e12efca4&page=${
-      this.state.page - 1
-    }&pageSize=${this.props.pageSize}`;
-    this.setState({ loading: true });
-    let data = await fetch(url);
-    let parsedData = await data.json();
-    console.log(parsedData);
-    this.setState({
-      page: this.state.page - 1,
-      articles: parsedData.articles,
-      loading: false,
-    });
+    this.setState({ page: this.state.page - 1 });
+    this.updateNews();
   };
 
   handleNextClick = async () => {
-    console.log("Next");
-    if (
-      !(
-        this.state.page + 1 >
-        Math.ceil(this.state.totalResults / this.props.pageSize)
-      )
-    ) {
-      let url = `https://newsapi.org/v2/top-headlines?country=${
-        this.props.country
-      }&category=${
-        this.props.category
-      }&apiKey=143400755624442599d36dc7e12efca4&page=${
-        this.state.page + 1
-      }&pageSize=${this.props.pageSize}`;
-      this.setState({ loading: true });
-      let data = await fetch(url);
-      let parsedData = await data.json();
-      console.log(parsedData);
-      this.setState({
-        page: this.state.page + 1,
-        articles: parsedData.articles,
-        loading: false,
-      });
-    }
+    this.setState({ page: this.state.page + 1 });
+    this.updateNews();
   };
 
   render() {
