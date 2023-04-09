dbd3679045702515668b04874461ecc2d5053f6b
Paarth <paarthkothari14@gmail.com>
2023-04-09 22:22:13 +0530

    revert back to original mode

---
+++ src/App.js
@@ -0,0 +1,105 @@
+import React, { useRef } from "react";
+import NavBar from "./components/NavBar";
+import News from "./components/News";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import LoadingBar from "react-top-loading-bar";
+
+const App = () => {
+  const ref = useRef(null);
+  // const [progress, setProgress] = useState(0);
+  let pageSize = 6;
+  let apiKey = process.env.REACT_APP_NEWS_API;
+
+  return (
+    <div className="text-center">
+      <Router basename="/React">
+        <NavBar />
+        <LoadingBar height={3} color="#f11946" ref={ref} />
+        <Switch>
+          <Route exact path="/">
+            <News
+              setProgress={ref}
+              apiKey={apiKey}
+              key="general"
+              pageSize={pageSize}
+              country="in"
+              category="general"
+            />
+          </Route>
+          <Route exact path="/business">
+            <News
+              setProgress={ref}
+              apiKey={apiKey}
+              key="business"
+              pageSize={pageSize}
+              country="in"
+              category="business"
+            />
+          </Route>
+          <Route exact path="/entertainment">
+            <News
+              setProgress={ref}
+              apiKey={apiKey}
+              key="entertainment"
+              pageSize={pageSize}
+              country="in"
+              category="entertainment"
+            />
+          </Route>
+          <Route exact path="/general">
+            <News
+              setProgress={ref}
+              apiKey={apiKey}
+              key="general"
+              pageSize={pageSize}
+              country="in"
+              category="general"
+            />
+          </Route>
+          <Route exact path="/health">
+            <News
+              setProgress={ref}
+              apiKey={apiKey}
+              key="health"
+              pageSize={pageSize}
+              country="in"
+              category="health"
+            />
+          </Route>
+          <Route exact path="/science">
+            <News
+              setProgress={ref}
+              apiKey={apiKey}
+              key="science"
+              pageSize={pageSize}
+              country="in"
+              category="science"
+            />
+          </Route>
+          <Route exact path="/sports">
+            <News
+              setProgress={ref}
+              apiKey={apiKey}
+              key="sports"
+              pageSize={pageSize}
+              country="in"
+              category="sports"
+            />
+          </Route>
+          <Route exact path="/technology">
+            <News
+              setProgress={ref}
+              apiKey={apiKey}
+              key="technology"
+              pageSize={pageSize}
+              country="in"
+              category="technology"
+            />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
+};
+
+export default App;
